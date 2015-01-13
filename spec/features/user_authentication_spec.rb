e739dc7b1f23ffc3b464647bbc1ef0e3ae613d2f
Chris Brett <c.brett@icloud.com>
2015-01-13 16:51:49 +0000

    User authentication test passing all three suggested tests

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -25,4 +25,23 @@ feature "User Authentication", :type => :feature do
 
   end
 
+  scenario "forgotten password" do
+    create(:user, email: "tester@example.tld", password: "test-password")
+
+    visit "/"
+    click_link "Sign In"
+
+    expect(current_path).to eq(new_user_session_path)
+    click_link "Forgot your password?"
+
+    expect(current_path).to eq(new_user_password_path)
+    fill_in "Email", with: "tester@example.tld"
+    click_button "Reset password"
+
+    expect(current_path).to eq(new_user_session_path)
+    expect(page).to have_content(
+      "You will receive an email with instructions on how to reset your password in a few minutes."
+    )
+  end
+
 end
