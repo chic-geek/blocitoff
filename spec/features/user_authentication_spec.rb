f2f5d8abd920f4d3e05b9d2ee85c77898ebf788c
Chris Brett <c.brett@icloud.com>
2015-01-14 19:44:38 +0000

    Complete user authentication reset password test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -22,6 +22,7 @@ feature "User Authentication", :type => :feature do
 
     expect(current_path).to eq("/")
     expect(page).to have_content("Signed out successfully.")
+    expect(page).not_to have_content("Hello, tester@example.tld")
 
   end
 
@@ -42,6 +43,18 @@ feature "User Authentication", :type => :feature do
     expect(page).to have_content(
       "You will receive an email with instructions on how to reset your password in a few minutes."
     )
+
+    open_email "tester@example.tld", with_subject: "Reset password instructions"
+    visit_in_email "Change my password"
+
+    expect(current_path).to eq(edit_user_password_path)
+    fill_in "New password", with: "new-password"
+    fill_in "Confirm new password", with: "new-password"
+    click_button "Change my password"
+
+    expect(current_path).to eq("/")
+    expect(page).to have_content("Your password has been changed successfully. You are now signed in.")
+    expect(page).to have_content("Hello, tester@example.tld")
   end
 
 end
