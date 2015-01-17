f21291caa7e8111fdaff37c3a342fd71dcb7f9b8
Chris Brett <c.brett@icloud.com>
2015-01-17 11:50:13 +0000

    Feature added: Ability to CRUD lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,42 @@ class ListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @list = current_user.list
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params.require(:list).permit(:title))
+    if @list.save
+      redirect_to lists_path, :notice => "Your list was saved"
+    else
+      render "new"
+    end
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update_attributes(params.require(:list).permit(:title))
+      redirect_to lists_path, :notice => "Your list has been updated."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, :notice => "Your list has been deleted"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, except: [:show]
+  resources :lists
   devise_for :users
   root to: 'welcome#index'
 end
