ffce19a868544a96096f116d1dddee41dd84335b
Chris Brett <c.brett@icloud.com>
2015-01-18 22:44:49 +0000

    WIP - To-do list items feature initiated

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_one :user
+  has_many :items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/new'
+
   resources :lists
+    resources :items, only:[:create]
   devise_for :users
   root to: 'welcome#index'
 end
