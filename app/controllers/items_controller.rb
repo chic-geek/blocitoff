7981651775413d0d6395a4c0e8d1bd6dd5af57a8
Chris Brett <c.brett@icloud.com>
2015-01-24 18:29:29 +0000

    Amend to items_controller.rb regarding authentication as per Eliots comments on PR #3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @list = List.find(params[:list_id])
+    @list = current_user.lists.find(params[:list_id])
     @item = Item.new(params.require(:item).permit(:name))
     @item.list_id = @list.id
     if @item.save
-      redirect_to [@list], :notice =>  "Your to-do was created."
+      redirect_to @list, :notice =>  "Your to-do was created."
     else
       render "new"
     end
