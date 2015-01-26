b8e0b5f2acaf75d8e9ca6046fcf0422d5f26f71a
Chris Brett <c.brett@icloud.com>
2015-01-26 13:02:21 +0000

    [WIP] Created users and assign lists to them.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,27 @@ require 'faker'
 ## TODO: Assign items to a list.
 
 
+## Create users
+## ----------------------------------------------
+#
+3.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+
 ## Create lists
+## ----------------------------------------------
 #
 5.times do
   List.create!(
+    user:  users.sample,
     title: Faker::Lorem.sentence(3)
   )
 end
@@ -15,6 +32,7 @@ lists = List.all
 
 
 ## Create tasks
+## ----------------------------------------------
 #
 10.times do
   Item.create!(
@@ -22,6 +40,27 @@ lists = List.all
     name: Faker::Lorem.sentence(8)
   )
 end
+
+
+## Create a few test users
+## ----------------------------------------------
+#
+memberone = User.new(
+  name:     'Member One',
+  email:    'memberone@test.com',
+  password: 'memberone'
+)
+memberone.skip_confirmation!
+memberone.save
+
+membertwo = User.new(
+  name:     'Member Two',
+  email:   'membertwo@test.com',
+  password: 'membertwo'
+)
+membertwo.skip_confirmation!
+membertwo.save
+
 
 puts "Seeding finished."
 puts "#{List.count} lists created."
