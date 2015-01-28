da6fbf14d945139bb59570a487bf92b859117a9b
Chris Brett <c.brett@icloud.com>
2015-01-28 19:26:48 +0000

    Fix item destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
     @list = List.find(params[:list_id])
     @item = @list.items.find(params[:id])
     @item.destroy
-    redirect_to list_items_path :notice => "Your task has been completed"
+    redirect_to @list, :notice => "Your task has been completed"
   end
 end
